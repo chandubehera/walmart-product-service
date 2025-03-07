5fcaf72625d446468295415e05a86b93b0036c98
Chandrakanta Behera <chandrakantab@maveric-systems.com>
2025-03-08 00:42:01 +0530

    pushing to new repo walmart-product-service

---
+++ src/main/java/com/walmart/productservice/WalmartProductServiceApplication.java
@@ -0,0 +1,13 @@
+package com.walmart.productservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WalmartProductServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WalmartProductServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/walmart/productservice/WalmartProductServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.walmart.productservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WalmartProductServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
